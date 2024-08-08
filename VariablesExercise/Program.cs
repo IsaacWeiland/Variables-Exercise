d4222acc41bd406d2dc7a38512f317901f17e182
Isaac Weiland <iweiland7777@gmail.com>
2024-08-08 11:57:51 -0400

    Wrote about Jebidiah

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Jebidiah";
+            int myPatience = 0;
+            char hatedLtr = 'q';
+            bool doSee = true;
+            decimal someAmount = 2.40m;
+            double otherAmount = 9.6;
+            Console.WriteLine($"I am {myName} when do see letter {hatedLtr} is {doSee} my patience is {myPatience}.\nMy bank's interest rate " +
+                              $"is {otherAmount} and I get {someAmount} every month!");
         }
     }
 }
